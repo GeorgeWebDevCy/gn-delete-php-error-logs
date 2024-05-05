bb3719b7206774792f1b303254366ce39c1519a6
George Nicolaou <orionas.elite@gmail.com>
2024-05-05 20:57:49 +0300

    initial base no real code yet

---
+++ gn-delete-php-error-logs.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * GN Delete PHP Error Logs
+ *
+ * @package       GNDELETEPH
+ * @author        George Nicolaou
+ * @license       gplv2
+ * @version       1.0.0
+ *
+ * @wordpress-plugin
+ * Plugin Name:   GN Delete PHP Error Logs
+ * Plugin URI:    https://www.georgenicolaou.me/plugins/gn-delete-php-error-logs
+ * Description:   Automatically deletes PHP error logs once daily.
+ * Version:       1.0.0
+ * Author:        George Nicolaou
+ * Author URI:    https://www.georgenicolaou.me/
+ * Text Domain:   gn-delete-php-error-logs
+ * Domain Path:   /languages
+ * License:       GPLv2
+ * License URI:   https://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GN Delete PHP Error Logs. If not, see <https://www.gnu.org/licenses/gpl-2.0.html/>.
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+// Include your custom code here.
